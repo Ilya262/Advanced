be2c56290153ad37b0be471a9c904d1485e3b360
ilchik262 <ilchik262@gmail.com>
2021-06-21 15:37:27 +0300

    esson 10 finish

---
+++ Lesson10_Advanced/src/main/java/filter/CreateProductFilter.java
@@ -0,0 +1,36 @@
+package filter;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import Schared.FilterService;
+import domain.Role;
+
+
+@WebFilter("/product")
+public class CreateProductFilter implements Filter {
+
+ 
+	public FilterService filterService = FilterService.getFilterUser();
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	
+		filterService.doFilterValidation(request, response, chain, Arrays.asList(Role.USER));
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		
+	}
+
+}
